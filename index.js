aab44e67176415c012c8e9acdd349fdc3abc34b6
Luis Montes <luis@iceddev.com>
2016-04-11 17:57:10 -0700

    custom register sw

--- index.js
+++ index.js
@@ -14,5 +14,16 @@ module.exports = {
   },
   loadServiceWorker: function(context){
     console.log('loading service worker');
+  },
+  registerServiceWorker: function(){
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('service-worker.bundle.js')
+      .then(function(reg) {
+        console.log('Service Worker Detected', reg);
+
+      }).catch(function(err) {
+        console.log('No Service Worker Detected', err);
+      });
+    }
   }
 };
