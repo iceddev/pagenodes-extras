664e94470ec4d76cd081977a2ccc695482f42f9d
Luis Montes <luis@iceddev.com>
2016-06-07 15:55:40 -0700

    clean service worker code

--- index.js
+++ index.js
@@ -11,19 +11,5 @@ module.exports = {
   },
   backendReady: function(RED){
     console.log('backend ready');
-  },
-  loadServiceWorker: function(context){
-    console.log('loading service worker');
-  },
-  registerServiceWorker: function(){
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('service-worker.bundle.js')
-      .then(function(reg) {
-        console.log('Service Worker Detected', reg);
-
-      }).catch(function(err) {
-        console.log('No Service Worker Detected', err);
-      });
-    }
   }
 };
